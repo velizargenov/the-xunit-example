c4800926b14d9a4fbac784f9cb91c1d34af6d789
Velizar Genov <velizargenov@Velizars-MacBook-Pro.local>
2018-04-06 12:00:22 +0100

    Implement classes * TestCase * TestResult * TestSuite * WasRun * TestCaseTest

---
+++ wasrun.py
@@ -0,0 +1,21 @@
+from testcase import TestCase
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+        self.log = "setUp "
+
+    def testMethod(self):
+        self.wasRun = 1
+        self.log = self.log + "testMethod "
+
+    def testBrokenMethod(self):
+        raise Exception
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
